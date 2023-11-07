3b5e71ec9c66abf1bab07f5182992928a0f8c784
Oleksii Siianchuk <94801705+Jleha@users.noreply.github.com>
2023-11-07 20:45:12 +0200

    Added empty LoginPage

--- ImpactWPF/ImpactWPF/LoginPage.xaml.cs
+++ ImpactWPF/ImpactWPF/LoginPage.xaml.cs
@@ -16,14 +16,13 @@ using System.Windows.Shapes;
 namespace ImpactWPF
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for LoginPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoginPage : Page
     {
-        public MainWindow()
+        public LoginPage()
         {
             InitializeComponent();
-            mainFrame.Navigate(new LoginPage());
         }
     }
 }
