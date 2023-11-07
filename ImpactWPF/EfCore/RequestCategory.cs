40ed0f9adb1bfe2bdc1f21cbb8ba9390979313d2
Oleksii Siianchuk <94801705+Jleha@users.noreply.github.com>
2023-11-07 20:06:39 +0200

    updated-project

---
+++ ImpactWPF/EfCore/RequestCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EfCore;
+
+public partial class RequestCategory
+{
+    public int CategoryId { get; set; }
+
+    public string CategoryName { get; set; } = null!;
+
+    public virtual ICollection<Request> Requests { get; set; } = new List<Request>();
+}
---
+++ ImpactWPF/EfCore/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EfCore;
+
+public partial class User
+{
+    public int UserId { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string? MiddleName { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public string PhoneNumber { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public int RoleRef { get; set; }
+
+    public virtual ICollection<Request> Requests { get; set; } = new List<Request>();
+
+    public virtual Role RoleRefNavigation { get; set; } = null!;
+}
